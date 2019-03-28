c62adb7d482c6867d2064c078e89ebd077f04b3c
dongyu <tangchao36@163.com>
2019-03-28 15:28:15 +0800

    new mobile app program

---
+++ hit_sell_thing/android/app/src/main/java/com/example/hit_sell_thing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hit_sell_thing;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
